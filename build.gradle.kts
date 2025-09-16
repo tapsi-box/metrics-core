d4ffc7e0b1284ad089d0b634ecc6f7531dc86a85
mahdibohloul <mahdi.bohloul@tapsi.cab>
2025-09-16 10:51:11 +0330

    Remove Spring Boot plugin from build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
   kotlin("jvm") version "1.9.23"
   kotlin("plugin.spring") version "1.9.23"
-  id("org.springframework.boot") version "3.5.5"
   id("io.spring.dependency-management") version "1.1.7"
 
   id("com.vanniktech.maven.publish") version "0.34.0"
